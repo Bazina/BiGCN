916112f9c599ab694db33d5cbeda7eed297b4f35
bazina <youssefbazina7@gmail.com>
2023-12-22 21:51:09 +0200

    BiGCN.

--- main.sh
+++ main.sh
@@ -9,6 +9,6 @@ python /content/BiGCN/Process/getTwittergraph.py Twitter15
 #Generate graph data and store in /data/Twitter16graph
 #python /content/BiGCN/Process/getTwittergraph.py Twitter16
 #Reproduce the experimental results.
-#python /content/BiGCN/model/Weibo/BiGCN_Weibo.py 100
-python /content/BiGCN/model/Twitter/BiGCN_Twitter.py Twitter15 100
-#python /content/BiGCN/model/Twitter/BiGCN_Twitter.py Twitter16 100
+#python /content/BiGCN/model/Weibo/BiGCN_Weibo.py 1
+python /content/BiGCN/model/Twitter/BiGCN_Twitter.py Twitter15 1
+#python /content/BiGCN/model/Twitter/BiGCN_Twitter.py Twitter16 1
